5f80fede57de5dff24b6d9cbf9b60bbbe9438164
gayoung <gayoung1@ualberta.ca>
2013-05-15 15:53:27 -0600

    - added an icon for plugin - inserted code from subordinate plugin to allow for jqueryUI dialog popup instead of tinymce's - still not quite working

--- editor_plugin_src.js
+++ editor_plugin_src.js
@@ -7,153 +7,334 @@
  * License: http://tinymce.moxiecode.com/license
  * Contributing: http://tinymce.moxiecode.com/contributing
  */
-
 (function() {
-	var each = tinymce.each;
+    // Load plugin specific language pack
+    tinymce.PluginManager.requireLangPack('imagemapper');
 
-	tinymce.create('tinymce.plugins.ImagemapperPlugin', {
-		init : function(ed, url) {
-			var t = this;
+    tinymce.create('tinymce.plugins.ImagemapperPlugin', {
+        /**
+         * Initializes the plugin, this will be executed after the plugin has been created.
+         * This call is done before the editor instance has finished it's initialization so use the onInit event
+         * of the editor instance to intercept that event.
+         *
+         * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+         * @param {string} url Absolute URL to where the plugin is located.
+         */
+        init : function(ed, url) {
+            // Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceImagemapper');
+                       
+            ed.addCommand('mceImagemapper', function() {
+                var idNumber;
+                var indexNumber;
+                var lasttype;
+                var subFlag = ''; // to determine if the imagemapper plugin was activated by another imagemapper plugin editor
+                
+                var type = ed.editorId.split("_");
+                
+                idNumber= ed.editorId.split("-");
+                
+                if(type[1] == "theorem")
+                {
+                    if(type[2] == "content")
+                    {
+                        indexNumber = "statementtheoremcontent"
+                    }
+                    else if(type[2] == "part")
+                    {
+                        indexNumber = "parttheoremcontent"
+                    }
+                }
+                else if(type[1] == "info")
+                {
+                    indexNumber = "infocontent";
+                }
+                else if(type[1] == "imagemapper")
+                {
+                    var tempString = '';
+                    for(var i=1; i < idNumber.length-1; i++)
+                    {
+                        tempString += idNumber[i] + "-";
+                    }
+                    
+                    tempString += idNumber[idNumber.length-1];
+                    
+                    subFlag = tempString;
+                    
+                    lasttype = type[2].split("-");
 
-			t.editor = ed;
+                    indexNumber = type[1]+lasttype[0];
+                }
+                    
+                else
+                {
+                    lasttype = type[2].split("-");
 
-			// Register commands
-			ed.addCommand('mceImagemapper', function(ui) {
-				ed.windowManager.open({
-					file : url + '/imagemapper.htm',
-					width : ed.getParam('imagemapper_popup_width', 750),
-					height : ed.getParam('imagemapper_popup_height', 600),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
+                    indexNumber = type[1]+lasttype[0];
+                }
+                                
+                for(var i=1; i < idNumber.length-1; i++)
+                {
+                    indexNumber += idNumber[i] + "-";
+                }
+                    
+                indexNumber += idNumber[idNumber.length-1];
+                                
+                makeImagemapperDialog(ed, indexNumber, subFlag);
+               
+            });
 
-			ed.addCommand('mceInsertImagemapper', t._insertImagemapper, t);
+            // Add a node change handler, when no content is selected, the button is disabled,
+            // otherwise, the button is enabled.
+            ed.onNodeChange.add(function(ed, cm, n) {
+                if(ed.selection.getContent())
+                {
+                    cm.setDisabled('imagemapper', false);
+                }
+                else
+                {
+                    cm.setActive('imagemapper', false);
+                    cm.setDisabled('imagemapper', true);
+                }
+            });
+            
+            // Register imagemapper button
+            ed.addButton('imagemapper', {
+                title : 'imagemapper.desc',
+                cmd : 'mceImagemapper',
+                image : url + '/img/imagemapper.png'
+            });
+            
+        },
 
-			// Register buttons
-			ed.addButton('imagemapper', {title : 'imagemapper.desc', cmd : 'mceImagemapper'});
+        /**
+         * Creates control instances based in the incomming name. This method is normally not
+         * needed since the addButton method of the tinymce.Editor class is a more easy way of adding buttons
+         * but you sometimes need to create more complex controls like listboxes, split buttons etc then this
+         * method can be used to create those.
+         *
+         * @param {String} n Name of the control to create.
+         * @param {tinymce.ControlManager} cm Control manager to use inorder to create new control.
+         * @return {tinymce.ui.Control} New control instance or null if no control was created.
+         */
+        createControl : function(n, cm) {
+            return null;
+        },
 
-			ed.onPreProcess.add(function(ed, o) {
-				var dom = ed.dom;
+        /**
+         * Returns information about the plugin as a name/value array.
+         * The current keys are longname, author, authorurl, infourl and version.
+         *
+         * @return {Object} Name/value array containing information about the plugin.
+         */
+        getInfo : function() {
+            return {
+                longname : 'Imagemapper plugin',
+                author : 'Ga Young Kim',
+                authorurl : 'http://ualberta.ca ',
+                infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/imagemapper',
+                version : "1.0"
+            };
+        }
+    });
 
-				each(dom.select('div', o.node), function(e) {
-					if (dom.hasClass(e, 'mceTmpl')) {
-						each(dom.select('*', e), function(e) {
-							if (dom.hasClass(e, ed.getParam('imagemapper_mdate_classes', 'mdate').replace(/\s+/g, '|')))
-								e.innerHTML = t._getDateTime(new Date(), ed.getParam("imagemapper_mdate_format", ed.getLang("imagemapper.mdate_format")));
-						});
-
-						t._replaceVals(e);
-					}
-				});
-			});
-		},
-
-		getInfo : function() {
-			return {
-				longname : 'Imagemapper plugin',
-				author : 'Moxiecode Systems AB',
-				authorurl : 'http://www.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/imagemapper',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
-			};
-		},
-
-		_insertImagemapper : function(ui, v) {
-			var t = this, ed = t.editor, h, el, dom = ed.dom, sel = ed.selection.getContent();
-
-			h = v.content;
-
-			each(t.editor.getParam('imagemapper_replace_values'), function(v, k) {
-				if (typeof(v) != 'function')
-					h = h.replace(new RegExp('\\{\\$' + k + '\\}', 'g'), v);
-			});
-
-			el = dom.create('div', null, h);
-
-			// Find imagemapper element within div
-			n = dom.select('.mceTmpl', el);
-			if (n && n.length > 0) {
-				el = dom.create('div', null);
-				el.appendChild(n[0].cloneNode(true));
-			}
-
-			function hasClass(n, c) {
-				return new RegExp('\\b' + c + '\\b', 'g').test(n.className);
-			};
-
-			each(dom.select('*', el), function(n) {
-				// Replace cdate
-				if (hasClass(n, ed.getParam('imagemapper_cdate_classes', 'cdate').replace(/\s+/g, '|')))
-					n.innerHTML = t._getDateTime(new Date(), ed.getParam("imagemapper_cdate_format", ed.getLang("imagemapper.cdate_format")));
-
-				// Replace mdate
-				if (hasClass(n, ed.getParam('imagemapper_mdate_classes', 'mdate').replace(/\s+/g, '|')))
-					n.innerHTML = t._getDateTime(new Date(), ed.getParam("imagemapper_mdate_format", ed.getLang("imagemapper.mdate_format")));
-
-				// Replace selection
-				if (hasClass(n, ed.getParam('imagemapper_selected_content_classes', 'selcontent').replace(/\s+/g, '|')))
-					n.innerHTML = sel;
-			});
-
-			t._replaceVals(el);
-
-			ed.execCommand('mceInsertContent', false, el.innerHTML);
-			ed.addVisual();
-		},
-
-		_replaceVals : function(e) {
-			var dom = this.editor.dom, vl = this.editor.getParam('imagemapper_replace_values');
-
-			each(dom.select('*', e), function(e) {
-				each(vl, function(v, k) {
-					if (dom.hasClass(e, k)) {
-						if (typeof(vl[k]) == 'function')
-							vl[k](e);
-					}
-				});
-			});
-		},
-
-		_getDateTime : function(d, fmt) {
-				if (!fmt)
-					return "";
-
-				function addZeros(value, len) {
-					var i;
-
-					value = "" + value;
-
-					if (value.length < len) {
-						for (i=0; i<(len-value.length); i++)
-							value = "0" + value;
-					}
-
-					return value;
-				}
-
-				fmt = fmt.replace("%D", "%m/%d/%y");
-				fmt = fmt.replace("%r", "%I:%M:%S %p");
-				fmt = fmt.replace("%Y", "" + d.getFullYear());
-				fmt = fmt.replace("%y", "" + d.getYear());
-				fmt = fmt.replace("%m", addZeros(d.getMonth()+1, 2));
-				fmt = fmt.replace("%d", addZeros(d.getDate(), 2));
-				fmt = fmt.replace("%H", "" + addZeros(d.getHours(), 2));
-				fmt = fmt.replace("%M", "" + addZeros(d.getMinutes(), 2));
-				fmt = fmt.replace("%S", "" + addZeros(d.getSeconds(), 2));
-				fmt = fmt.replace("%I", "" + ((d.getHours() + 11) % 12 + 1));
-				fmt = fmt.replace("%p", "" + (d.getHours() < 12 ? "AM" : "PM"));
-				fmt = fmt.replace("%B", "" + this.editor.getLang("imagemapper_months_long").split(',')[d.getMonth()]);
-				fmt = fmt.replace("%b", "" + this.editor.getLang("imagemapper_months_short").split(',')[d.getMonth()]);
-				fmt = fmt.replace("%A", "" + this.editor.getLang("imagemapper_day_long").split(',')[d.getDay()]);
-				fmt = fmt.replace("%a", "" + this.editor.getLang("imagemapper_day_short").split(',')[d.getDay()]);
-				fmt = fmt.replace("%%", "%");
-
-				return fmt;
-		}
-	});
-
-	// Register plugin
-	tinymce.PluginManager.add('imagemapper', tinymce.plugins.ImagemapperPlugin);
+    // Register plugin
+    tinymce.PluginManager.add('imagemapper', tinymce.plugins.ImagemapperPlugin);
 })();
+
+function makeImagemapperDialog(ed, idNumber, isSub)
+{
+    var container = null;
+    var dialogwhole = document.createElement('div');
+    var dialogForm = document.createElement('form');
+    var dialogFormContainer = document.createElement('div');
+    var selectedTextlabel = document.createElement('label');
+    var selectedTextValue = document.createTextNode('Selected Text : ');
+    var selectedTextInput = document.createElement('input');
+    
+    var selectTypeLabel = document.createElement('label');
+    var selectTypeText = document.createTextNode('Imagemapper Type : ');
+    var selectTypeMenu = document.createElement('select');
+    
+    var selectTypeOption1 = document.createElement('option');
+    selectTypeOption1.value = "Information";
+    var selectTypeOption1Value = document.createTextNode('Information');
+    var selectTypeOption2 = document.createElement('option');
+    selectTypeOption2.setAttribute("value", "External Link");
+    var selectTypeOption2Value = document.createTextNode('External Link');
+    var selectTypeOption3 = document.createElement('option');
+    selectTypeOption3.setAttribute("value", "Internal Reference");
+    var selectTypeOption3Value = document.createTextNode('Internal Reference');
+    var selectTypeOption4 = document.createElement('option');
+    selectTypeOption4.setAttribute("value", "External Reference");
+    var selectTypeOption4Value = document.createTextNode('External Reference');
+    
+    var selectedNode = null;
+    
+    if($.browser.msie)
+    {
+        selectedNode = ed.selection.getNode().childNodes[0].tagName;
+    }
+    else
+    {
+        selectedNode = ed.selection.getNode().tagName;
+    }
+    
+    selectTypeOption1.setAttribute("selected", "selected");
+    
+    selectTypeOption1.appendChild(selectTypeOption1Value);
+    selectTypeOption2.appendChild(selectTypeOption2Value);
+    selectTypeOption3.appendChild(selectTypeOption3Value);
+    selectTypeOption4.appendChild(selectTypeOption4Value);
+    
+    var dialogContentForm = document.createElement('div');
+    var dialogButtonContainer = document.createElement('div');
+    var saveButton = document.createElement('input');
+    var cancelButton = document.createElement('input');
+    
+    if(isSub != '')
+    {
+        if(selectedNode != 'A')
+        {
+            container = document.createElement("div");
+            idNumber = isExistingIndex(idNumber+"-1");
+            
+            container.id = 'msm_imagemapper_container-'+idNumber;
+            container.className = 'msm_imagemapper_containers';
+             
+            var parentDiv = findParentDiv(isSub);
+            $(parentDiv).append(container);
+        }
+        else
+        {
+            var wordId = '';            
+            if($.browser.msie)
+            {
+                wordId = ed.selection.getNode().childNodes[0].id;
+            }
+            else
+            {
+                wordId = ed.selection.getNode().id;
+            }
+            
+            var pattern = /([A-Za-z]*?)(\d+)((?:-\d+)*)/;
+            
+            var editorIdInfo = ed.editorId.split("-");   
+            
+            var idReplacement = replaceIdEnding(ed, editorIdInfo);            
+            
+            var wordIdInfo = wordId.split("-");           
+            var oldString = '';
+            for(var i = 1; i < wordIdInfo.length-2; i++)
+            {
+                oldString += wordIdInfo[i]+"-";
+            }  
+            oldString += wordIdInfo[wordIdInfo.length-2];
+            
+            idNumber = oldString.replace(pattern, "$1"+idReplacement+"$3");   
+            
+            container = document.getElementById('msm_imagemapper_container-'+idNumber);
+        }
+    }
+    else
+    {
+        container = document.getElementById('msm_imagemapper_container-'+idNumber);
+    }    
+            
+    dialogwhole.id = 'msm_imagemapper-'+idNumber;
+    container.setAttribute("title", "Create Imagemapper");
+        
+    dialogForm.id = 'msm_imagemapper_form-'+idNumber;
+        
+    dialogFormContainer.className = "msm_imagemapper_form_container";
+        
+    selectedTextlabel.setAttribute("for",'msm_imagemapper_highlighted-'+idNumber);
+    selectedTextlabel.appendChild(selectedTextValue);
+        
+    selectedTextInput.id = 'msm_imagemapper_highlighted-'+idNumber;
+    selectedTextInput.name = 'msm_imagemapper_highlighted-'+idNumber;
+    selectedTextInput.setAttribute("disabled", "disabled");
+        
+    selectTypeLabel.setAttribute("for", 'msm_imagemapper_select-'+idNumber);
+    selectTypeLabel.appendChild(selectTypeText);
+        
+    selectTypeMenu.id = 'msm_imagemapper_select-'+idNumber;
+    selectTypeMenu.name = 'msm_imagemapper_select-'+idNumber;
+    selectTypeMenu.onchange = function(event) {
+        changeForm(event, ed, idNumber);
+    };
+        
+    dialogContentForm.id = 'msm_imagemapper_content_form_container-'+idNumber;
+    dialogContentForm.className = "msm_imagemapper_content_form_containers";
+        
+    dialogButtonContainer.className = 'msm_imagemapper_button_container';
+        
+    saveButton.setAttribute("type", "button");
+    saveButton.id = 'msm_imagemapper_submit-'+idNumber;
+    saveButton.className = 'msm_imagemapper_button';
+    saveButton.setAttribute("value", "Save");
+    saveButton.onclick = function() {
+        submitSubForm(ed, idNumber, isSub);
+    };
+        
+    cancelButton.setAttribute("type", "button");
+    cancelButton.id = 'msm_imagemapper_cancel-'+idNumber;
+    cancelButton.className = 'msm_imagemapper_button';
+    cancelButton.setAttribute("value", "Cancel");
+    cancelButton.onclick = function() {
+        closeSubFormDialog(idNumber);
+    };
+    
+    var infoForm = makeInfoForm(ed, idNumber);
+    dialogContentForm.appendChild(infoForm);
+    
+    selectTypeMenu.appendChild(selectTypeOption1);
+    selectTypeMenu.appendChild(selectTypeOption2);
+    selectTypeMenu.appendChild(selectTypeOption3);
+    selectTypeMenu.appendChild(selectTypeOption4);
+        
+    dialogButtonContainer.appendChild(saveButton);
+    dialogButtonContainer.appendChild(cancelButton);
+        
+    dialogFormContainer.appendChild(selectedTextlabel);
+    dialogFormContainer.appendChild(selectedTextInput);
+    dialogFormContainer.appendChild(document.createElement('br'));
+    dialogFormContainer.appendChild(document.createElement('br'));
+    dialogFormContainer.appendChild(selectTypeLabel);
+    dialogFormContainer.appendChild(selectTypeMenu);
+    dialogFormContainer.appendChild(dialogContentForm);
+        
+    dialogForm.appendChild(dialogFormContainer);
+    dialogForm.appendChild(document.createElement('br'));
+    dialogForm.appendChild(document.createElement('br'));
+    dialogForm.appendChild(dialogButtonContainer);
+        
+    dialogwhole.appendChild(dialogForm);
+      
+    // only append the new dialog form to div when it hasn't already been done
+//    if(!container.hasChildNodes())
+//    {
+//        container.appendChild(dialogwhole);    
+//    }
+//    else
+//    {
+//        $('#msm_imagemapper_container-'+idNumber+" textarea").each(function() {
+//            if(tinymce.getInstanceById($(this).attr("id")) != null)
+//            {
+//                tinymce.execCommand('mceFocus', false, $(this).attr("id"));
+//                tinymce.execCommand('mceRemoveControl', false, $(this).attr("id"));
+//            }
+//        });
+//        $('#msm_imagemapper_container-'+idNumber).empty();
+//        container.appendChild(dialogwhole);
+//    }
+//    
+//    if(selectedNode == "A")
+//    {
+//        changeSelectIndex(ed, idNumber);
+//        loadPreviousData(ed, idNumber);
+//    }
+    
+    createDialog(ed, idNumber);
+    
+}
+
